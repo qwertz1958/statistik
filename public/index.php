ed6acba646ac02a515e69d6090db6d7e0de33c4a
dominik <dom.schmidt1986@gmail.com>
2021-05-31 12:45:35 +0200

    [ng-348]: Routing

--- public/index.php
+++ public/index.php
@@ -9,3 +9,26 @@
 
 // Autoloader
 include_once ('../vendor/autoload.php');
+
+$app = new \Slim\App();
+
+// Route
+$app->get('/', function ($request, $response, $args){
+    $response->write('Welcome to Slim crash course from FrontendTips');
+});
+
+// Json rückgabe
+$countries = array(
+    array('name' => 'USA'),
+    array('name' => 'Germany'),
+    array('name' => 'Poland'),
+    array('name' => 'Usbekistan')
+);
+
+$app->get('/countries', function($request, $response, $args) use ($countries){
+    return $response->withJson($countries);
+});
+
+$app->run();
+
+?>
