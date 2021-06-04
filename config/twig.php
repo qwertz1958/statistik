256bc74373e1a25f322ce178c2c0b55ca58f191a
dominik <dom.schmidt1986@gmail.com>
2021-06-04 09:01:24 +0200

    [ng-348]: einfügen twig

---
+++ config/twig.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Konfiguration Twig View
+ *
+ * 04.06.2021
+ * arise
+ * twig.php
+ */
+
+$container['view'] = function ($container)
+{
+    $pathToTemplate = realpath('../public/template');
+    $view = new \Slim\Views\Twig($pathToTemplate, [
+        'cache' => false
+    ]);
+    // Instantiate and add Slim specific extension
+    $router = $container->get('router');
+    $uri = \Slim\Http\Uri::createFromEnvironment(new \Slim\Http\Environment($_SERVER));
+    $view->addExtension(new \Slim\Views\TwigExtension($router, $uri));
+    return $view;
+};
