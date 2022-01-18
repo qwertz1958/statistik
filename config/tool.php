8d05eaec7477ef5d04857ae1c59b9b1e7d5b3379
Stephan Krauss <info@stephankrauss.de>
2022-01-18 16:07:33 +0100

    [develop] Einbau Statistik Klasse

--- config/tool.php
+++ config/tool.php
@@ -1,1 +1,13 @@
 <?php
+
+// Exception Statistik Klasse
+$container[\App\Tool\StatisticsError::class] = function($container)
+{
+	return new App\Tool\StatisticsError();
+};
+
+// Statistik Klasse
+$container[\App\Tool\Statistics::class] = function($container)
+{
+    return new App\Action\Statistics();
+};
