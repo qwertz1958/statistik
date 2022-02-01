07c0d629776354fe8e8fc3aada8356bf0000b64e
Stephan <info@stephankrauss.de>
2022-02-01 12:11:09 +0100

    [develop] individuelle Farben

--- config/navigation.php
+++ config/navigation.php
@@ -9,6 +9,7 @@ $container['navigation'] = function($container)
 	$navigation[1] = ['link' => '/view/tabelle', 'linkText' => 'Tabelle'];
 	$navigation[2] = ['link' => '/view/charts', 'linkText' => 'Diagramme'];
 	$navigation[3] = ['link' => '/view/test', 'linkText' => 'Test'];
+	$navigation[4] = ['link' => '/view/test1', 'linkText' => 'Test 1'];
 
 	return $navigation;
 };
