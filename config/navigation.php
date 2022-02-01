a7d0e93d260e799d4c7ce749585657142a0bb47b
Stephan <info@stephankrauss.de>
2022-02-01 11:56:12 +0100

    [develop]  Test Chart

--- config/navigation.php
+++ config/navigation.php
@@ -8,6 +8,7 @@ $container['navigation'] = function($container)
 	$navigation[0] = ['link' => '/view/home','linkText' => 'Projektbeschreibung'];
 	$navigation[1] = ['link' => '/view/tabelle', 'linkText' => 'Tabelle'];
 	$navigation[2] = ['link' => '/view/charts', 'linkText' => 'Diagramme'];
+	$navigation[3] = ['link' => '/view/test', 'linkText' => 'Test'];
 
 	return $navigation;
 };
