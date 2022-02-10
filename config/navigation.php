e81db6354de2095aaaf35c9befa17d5f1192c10e
Stephan <info@stephankrauss.de>
2022-02-10 17:43:09 +0100

    [develop]  2. Demonstrationsvideo: Installation, Programmaufbau und Datenbasis

--- config/navigation.php
+++ config/navigation.php
@@ -12,6 +12,7 @@ $container['navigation'] = function($container)
 	$navigation[] = ['link' => '/view/chart1', 'linkText' => 'Balkendiagramm'];
 	$navigation[] = ['link' => '/view/chart2', 'linkText' => 'Zufallswerte'];
 	$navigation[] = ['link' => '/view/chart3', 'linkText' => 'Baumbestand'];
+	$navigation[] = ['link' => '/view/chart4', 'linkText' => 'DDDD'];
 
 	return $navigation;
 };
